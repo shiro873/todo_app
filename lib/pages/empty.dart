d8d7caab07e492d45e230ab2df54ebc9c7fa4854
Ridwan Shuvro <shuvro_r@hotmail.com>
2022-02-14 20:08:08 +0600

    initial UI commit

---
+++ lib/pages/empty.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Empty extends StatefulWidget {
+  Empty({Key? key}) : super(key: key);
+
+  _EmptyState createState() => _EmptyState();
+}
+
+class _EmptyState extends State<Empty> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(),
+    );
+  }
+}
---
+++ lib/widgets/fab.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// FloatingActionButton customFab(context) {
+//   Modal modal = Modal();
+// }
